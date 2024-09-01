476cf7d337a08191538de2df10edb5aa2ab8340f
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2024-09-01 16:42:39 +0200

    update catalogue page

--- src/app/catalogue/page.js
+++ src/app/catalogue/page.js
@@ -1,11 +1,9 @@
-import Cart from '../components/Cart/Cart';
 import ProductCard from '../components/ProductCard/ProductCard';
 
 export default function Home() {
   return (
     <main>
       <ProductCard />
-      <Cart />
     </main>
   );
 }
